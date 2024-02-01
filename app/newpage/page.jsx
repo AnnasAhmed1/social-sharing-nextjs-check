2b12b7d15036237e6c9f42074576b24d8139a8f6
AnnasAhmed1 <annasahmed1609@gmail.com>
2024-02-01 18:06:43 +0500

    image-update

--- app/newpage/page.jsx
+++ app/newpage/page.jsx
@@ -5,7 +5,7 @@ export const metadata = {
   description: "New page Description",
   openGraph: {
     images:
-      "https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I=",
+      "https://media.istockphoto.com/id/1211781372/photo/sityscape-of-beautiful-metropolitan-city-karachi.jpg?s=612x612&w=0&k=20&c=s8x-7guoOpjclH2gnW1Eh6hE9Zj54YnIIm6vfY6C3Ig=",
   },
 };
 const YourPage = () => {
